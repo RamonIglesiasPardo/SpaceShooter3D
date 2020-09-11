75483ee7e5ce0c2963f4902fc1ec9f4d023df22e
Ramón Iglesias <iglesias.ramon@outlook.com>
2020-09-11 19:25:36 +0200

    Add a cylinder as background with dynamic texture. Add test ship model.

---
+++ Assets/Scripts/BackGroundController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundController : MonoBehaviour
+{
+    new Renderer renderer;
+    public float panSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(0.0f, Time.time * panSpeed);
+        renderer.material.mainTextureOffset = offset;
+    }
+}
