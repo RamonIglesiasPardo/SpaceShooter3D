d196407370a894a940dbba5e5476afc8206b85b0
Stradmann <57647277+Stradmann@users.noreply.github.com>
2020-09-14 12:53:13 +0200

    Change SelfDestruction, add formations mechanics, add new ship

---
+++ Assets/BasicBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBehavior : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector3.back * speed;
+    }
+}
