af0fbb6ebe314f7394a29b55cafd63768fa69637
Ramón Iglesias <iglesias.ramon@outlook.com>
2020-09-20 19:40:09 +0200

    Refactor formations. Fit to playable area.

--- Assets/FormationConstructor.cs
+++ Assets/FormationConstructor.cs
@@ -1,145 +1,72 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class FormationConstructor : MonoBehaviour
 {
-    private PlayerShipController playerShipController;
-
     public string[] formations;
     public List<Vector3> positionsToSpawn = new List<Vector3>();
-    public List<Vector3> spawneablePositions = new List<Vector3>();
 
-    void Start()
-    {       
-        playerShipController = GameObject.FindGameObjectWithTag("PlayerShip").GetComponent<PlayerShipController>();
-    }
-
-    public void Formation(GameObject element, Transform spawnPoint)
+    public void Formation(GameObject element, List<Vector3> spawneablePositions)
     {
-        float anchoArea = (new Vector2(playerShipController.xMax, 0.0f) - new Vector2(playerShipController.xMin, 0.0f)).magnitude;
-        float altoArea = (new Vector2(0.0f, playerShipController.yMax) - new Vector2(0.0f, playerShipController.yMin)).magnitude;
-        float anchoNave = 1f;
-        float altoNave = 1f;
-        float restoAncho = anchoArea % anchoNave;
-        float restoAlto = altoArea % altoNave;
-        float navesMaxX = (anchoArea - restoAncho) / anchoNave;
-        float navesMaxY = (altoArea - restoAlto) / altoNave;
         bool isAsteroid = element.CompareTag("Asteroid");
-        //Debug.Log(anchoArea + ", " + altoArea + "; " + restoAncho + ", " + restoAlto + ";" + navesMaxX + "," + navesMaxY);
-
-        Vector3 startPosition = spawnPoint.position;
-        spawneablePositions.Add(startPosition);
-        Vector3 newPosition;
-
-        newPosition = startPosition;
-        LlenarX(newPosition, navesMaxX, anchoNave);
-        //llenamos la Y
-        for (int i = 1; i <= navesMaxY; i++)
-        {
-            newPosition.y += altoNave;
-            if (InsideLimits(newPosition))
-            {
-                spawneablePositions.Add(newPosition);
-            }
-            LlenarX(newPosition, navesMaxX, anchoNave);
-        }
-        newPosition = startPosition;
-        for (int i = 1; i <= navesMaxY; i++)
-        {
-            newPosition.y -= altoNave;
-            if (InsideLimits(newPosition))
-            {
-                spawneablePositions.Add(newPosition);
-            }
-            LlenarX(newPosition, navesMaxX, anchoNave);
-        }
-        int navesEnX = 0;
-        foreach (Vector3 position in spawneablePositions)
-        {
-            if (spawneablePositions[0].y == position.y)
-            {
-                navesEnX++;
-            }
-        }
-        float margenParaLlenarX = restoAncho / navesEnX;
-        for (int i = 0; i < spawneablePositions.Count; i++)
-        {
-            Vector3 vector = spawneablePositions[i];
-            vector.x = spawneablePositions[i].x + margenParaLlenarX;
-            spawneablePositions[i] = vector;
-        }
-        
-        //----------------------------------------------//
-        //Escogemos Formación   //y = (-3/7)x + (1/2)
-        string selectedFormation = formations[Random.Range(0, formations.Length)];
-        Debug.Log(selectedFormation);
+        string[] asteroidsFormations = {"Single", "Random" };
+        string[] shipsFormations = { "Complete", "HLine", "VLine", "Cross", "LeftDownTri", "RightDownTri", "LeftUpTri", "RightUpTri" };
+        string selectedFormation = isAsteroid? asteroidsFormations[Random.Range(0, asteroidsFormations.Length)] : shipsFormations[Random.Range(0, shipsFormations.Length)];
+        //selectedFormation = "Complete";
         switch (selectedFormation)
         {
             case "Single":
-                positionsToSpawn.Add(startPosition);
+                positionsToSpawn.Add(spawneablePositions[Random.Range(0, spawneablePositions.Count)]);
                 break;
-
             case "Random":
-                int numOfShips = Random.Range(1, 7);
-                for (int i = 0; i < numOfShips; i++)
+                int numOfElements = Random.Range(4, spawneablePositions.Count);
+                for (int i = 0; i < numOfElements; i++)
                 {
-                    Vector3 choosenPosition = spawneablePositions[Random.Range(0, spawneablePositions.Count)];
-                    if (!positionsToSpawn.Contains(choosenPosition))
-                    {
-                        positionsToSpawn.Add(choosenPosition);
-                    }
+                    Vector3 randomFormationElement = spawneablePositions[Random.Range(0, spawneablePositions.Count)];
+                    if (!positionsToSpawn.Contains(randomFormationElement)) positionsToSpawn.Add(randomFormationElement);
                 }
                 break;
-
             case "Complete":
-                foreach (Vector3 position in spawneablePositions)
+                foreach (Vector3 completeFormationElement in spawneablePositions)
                 {
-                    positionsToSpawn.Add(position);
+                    positionsToSpawn.Add(completeFormationElement);
                 }
                 break;
-
             case "HLine":
-                foreach(Vector3 position in spawneablePositions)
-                {
-                    if (position.y == startPosition.y)
-                    {
-                        positionsToSpawn.Add(position);
-                    }
-                }
-                break;
-
-            case "VLine":
-                foreach(Vector3 position in spawneablePositions)
-                {
-                    if (position.x == startPosition.x)
-                    {
-                        positionsToSpawn.Add(position);
-                    }
-                }
-                break;
-
-            case "Cross":
-                foreach(Vector3 position in spawneablePositions)
-                {
-                    if (position.x == startPosition.x || position.y == startPosition.y)
-                    {
-                        positionsToSpawn.Add(position);
-                    }
-                }
-                break;
-
-            case "LeftDownTri":
+                Vector3 hLineFormationElement = spawneablePositions[Random.Range(0, spawneablePositions.Count)];
                 foreach (Vector3 position in spawneablePositions)
                 {
-                    float y = (-3.0f / 7.0f) * position.x + (1.0f / 2.0f);
-                    if (position.y <= y)
-                    {
-                        positionsToSpawn.Add(position);
-                    }
+                    if (hLineFormationElement.y == position.y) positionsToSpawn.Add(position);
                 }
                 break;
-
+            case "VLine":
+                Vector3 vLineFormationElement = spawneablePositions[Random.Range(0, spawneablePositions.Count)];
+                foreach (Vector3 position in spawneablePositions)
+                {
+                    if (vLineFormationElement.x == position.x) positionsToSpawn.Add(position);
+                }
+                break;
+            case "Cross":
+                Vector3 crossFormationElement = spawneablePositions[Random.Range(0, spawneablePositions.Count)];
+                foreach (Vector3 position in spawneablePositions)
+                {
+                    if (crossFormationElement.y == position.y || crossFormationElement.x == position.x) positionsToSpawn.Add(position);
+                }
+                break;
+            case "LeftDownTri":
+                int colZeroCount = 0;
+                for (int i = 0; i < spawneablePositions.Count; i++)
+                {
+                    if (i == 0 || spawneablePositions[i].x != spawneablePositions[i-1].x)
+                    {
+                        positionsToSpawn.Add(spawneablePositions[i]);                        
+                    }
+                    if (spawneablePositions[i].x == spawneablePositions[0].x)
+                    {
+                        colZeroCount++; 
+                    }
+                }                
+                break;
             case "RightDownTri":
                 foreach (Vector3 position in spawneablePositions)
                 {
@@ -150,7 +77,6 @@ public class FormationConstructor : MonoBehaviour
                     }
                 }
                 break;
-
             case "LeftUpTri":
                 foreach (Vector3 position in spawneablePositions)
                 {
@@ -161,7 +87,6 @@ public class FormationConstructor : MonoBehaviour
                     }
                 }
                 break;
-
             case "RightUpTri":
                 foreach (Vector3 position in spawneablePositions)
                 {
@@ -171,44 +96,7 @@ public class FormationConstructor : MonoBehaviour
                         positionsToSpawn.Add(position);
                     }
                 }
-                break;
-        }
-
+                break;        }
         spawneablePositions.Clear();
-    }
-
-    void LlenarX(Vector3 posicionEnY, float maxNavesX, float anchoNave)
-    {
-        Vector3 newXPosition = posicionEnY;
-        for (int i = 1; i <= maxNavesX; i++)
-        {
-            newXPosition.x += anchoNave;
-            if (InsideLimits(newXPosition))
-            {
-                spawneablePositions.Add(newXPosition);
-            }
-        }
-        newXPosition = posicionEnY;
-        for (int i = 1; i <= maxNavesX; i++)
-        {
-            newXPosition.x -= anchoNave;
-            if (InsideLimits(newXPosition))
-            {
-                spawneablePositions.Add(newXPosition);
-            }
-        }
-    }
-
-    bool InsideLimits(Vector3 position)
-    {
-        if (position.x > playerShipController.xMin && position.x < playerShipController.xMax
-            && position.y > playerShipController.yMin && position.y < playerShipController.yMax)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
     }
 }
--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -1,52 +1,80 @@
-﻿using Boo.Lang;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
     public int level;
     public float waveStart;
     public float spawnRate;
-    private float wave;
+    private float timeToStartNextWave;
 
     private FormationConstructor formations;
     private GameObject[] enemies;
     private GameObject[] asteroids;
     private PlayerShipController playerShipController;
-    public Transform spawnPoint;
 
     private void Start()
     {
         playerShipController = GameObject.FindGameObjectWithTag("PlayerShip").GetComponent<PlayerShipController>();
         enemies = Resources.LoadAll<GameObject>("Enemies");
         asteroids = Resources.LoadAll<GameObject>("Asteroids/Prefabs");
-        wave = Time.time + waveStart;
+        timeToStartNextWave = Time.time + waveStart;
         formations = GetComponent<FormationConstructor>();
     }
 
     private void Update()
     {
-        if (Time.time > wave)
+        if (Time.time > timeToStartNextWave && Application.isPlaying)
         {            
             GameObject element = (IsHardEnemy()) ? enemies[Random.Range(0, enemies.Length)] : asteroids[Random.Range(0, asteroids.Length)];
-            spawnPoint.position = new Vector3(playerShipController.xMin, playerShipController.yMin, spawnPoint.position.z);
-            SpawnWave(element);
-            wave = Time.time + spawnRate;
+            List <Vector3> spawndablesPositionForElement = GetSpawndablesPositionForElement(element, 0.3f);
+            SpawnWave(element , spawndablesPositionForElement);
+            timeToStartNextWave = Time.time + spawnRate;
         }
     }
 
+    private List<Vector3> GetSpawndablesPositionForElement(GameObject element, float minMarginBetweenElements)
+    {
+        float playableAreaWidth = (new Vector2(playerShipController.xMax, 0.0f) - new Vector2(playerShipController.xMin, 0.0f)).magnitude;
+        float playableAreaHeight = (new Vector2(0.0f, playerShipController.yMax) - new Vector2(0.0f, playerShipController.yMin)).magnitude;
+        float elementWidth = element.GetComponent<Renderer>().bounds.size.x;
+        float elementHeight = element.GetComponent<Renderer>().bounds.size.y;
+        //float minMarginBetweenElements = element.GetComponent<Renderer>().bounds.size.x/5;
+        float remaindereWidth = playableAreaWidth % (elementWidth + minMarginBetweenElements);
+        float remainderHeight = playableAreaHeight % (elementHeight + minMarginBetweenElements);
+        float maxShipsX = (float) Math.Floor(playableAreaWidth / (elementWidth + minMarginBetweenElements));
+        float maxShipsY = (float) Math.Floor(playableAreaHeight / (elementHeight + minMarginBetweenElements));
+        float marginBetweenShipsX = remaindereWidth / maxShipsX;
+        float marginBetweenShipsY = remainderHeight / maxShipsY;        
+        List <Vector3> spawndablesPositionForElement = new List<Vector3>();
+        for (int i = 0; i <= maxShipsX; i++)
+        {
+            float x = playerShipController.xMin + ((elementWidth + marginBetweenShipsX + minMarginBetweenElements) * i);
+            spawndablesPositionForElement.Add(new Vector3(x, playerShipController.yMin, this.transform.position.z));            
+            for (int j = 1; j <= maxShipsY; j++)
+            {
+                float y = playerShipController.yMin + ((elementHeight + marginBetweenShipsY + minMarginBetweenElements) * j);
+                spawndablesPositionForElement.Add(new Vector3(x, y, this.transform.position.z));
+            }
+        }
+        return spawndablesPositionForElement;
+    }    
+
     private bool IsHardEnemy()
     {
-        int probabilityEnemy = System.Math.Min(level * 1, 9);
+        int probabilitySpawnHardEnemy = System.Math.Min(level, 8);
         int randomSelection = Random.Range(0, 10);
-        return randomSelection <= probabilityEnemy;
+        return randomSelection <= probabilitySpawnHardEnemy;
     }
 
-    void SpawnWave(GameObject element)
+    void SpawnWave(GameObject element, List<Vector3> spawndablesPositionForElement)
     {
-        formations.Formation(element, spawnPoint);
+        formations.Formation(element, spawndablesPositionForElement);
         foreach (Vector3 position in formations.positionsToSpawn)
         {
-            Instantiate(element, position, spawnPoint.rotation);
+            Instantiate(element, position, this.transform.rotation);
         }
         formations.positionsToSpawn.Clear();
     }
