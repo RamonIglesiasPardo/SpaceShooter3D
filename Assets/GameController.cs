47952161b11d0efe8cf386368655ef0cf3f3b1be
Stradmann <57647277+Stradmann@users.noreply.github.com>
2020-09-23 18:07:54 +0200

    add pause button

---
+++ Assets/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public PauseButton pauseButton;
+
+    private void Update()
+    {
+        if (pauseButton.pauseIsPressed)
+        {
+            Time.timeScale = 0.0f;
+            Debug.Log("Game paused, in theory");
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
+}
---
+++ Assets/PauseButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PauseButton : MonoBehaviour, IPointerDownHandler
+{
+    [HideInInspector]
+    public bool pauseIsPressed = false;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pauseIsPressed = !pauseIsPressed;
+        Debug.Log(pauseIsPressed);
+    }
+}
